05c8ee005d1487c589d6f15903b7bd1c10640c78
alexr415 <121706778+alexr415@users.noreply.github.com>
2024-09-25 23:43:54 -0700

    started on api - added users/ page

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
+from views import get_users
 urlpatterns = [
     path('',views.homepage),
+    path('users/',get_users,name='get_user'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
+
 
 # Create your views here.
 def homepage(request):
     return render(request,'homepage.html')
+
+@api_view(['GET'])
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many= True)
+    return Response(serializer.data)
