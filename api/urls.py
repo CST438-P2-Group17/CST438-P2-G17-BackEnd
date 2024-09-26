ce52610ac38c8c23dd78b00c709a23825281ce9b
alexr415 <121706778+alexr415@users.noreply.github.com>
2024-09-25 23:50:05 -0700

    mightve fixed error

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from views import get_users
+from .views import get_users
 urlpatterns = [
     path('',views.homepage),
     path('users/',get_users,name='get_user'),
