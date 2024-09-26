a966f60e45fd236370277a8703ebd166da39ef03
alexr415 <121706778+alexr415@users.noreply.github.com>
2024-09-26 00:06:30 -0700

    added post put and delete to api

--- api/views.py
+++ api/views.py
@@ -15,3 +15,31 @@ def get_users(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many= True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def user_detail(request,pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer= UserSerializer(user)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
