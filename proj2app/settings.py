379a63571f56de22ecc3f0e85e9d5366e2973725
alexr415 <121706778+alexr415@users.noreply.github.com>
2024-09-26 17:11:33 -0700

    moving db to JawsDB Maria

--- proj2app/settings.py
+++ proj2app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +78,9 @@ WSGI_APPLICATION = 'proj2app.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('JAWSDB_MARIA_URL')
+    )
 }
 
 
