92f1b8b3ffaa02a9ba20ba03def6b5f30a081262
alexr415 <121706778+alexr415@users.noreply.github.com>
2024-09-26 17:21:53 -0700

    Set STATIC_ROOT for collectstatic

--- proj2app/settings.py
+++ proj2app/settings.py
@@ -14,10 +14,13 @@ from pathlib import Path
 import dj_database_url
 import os
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
