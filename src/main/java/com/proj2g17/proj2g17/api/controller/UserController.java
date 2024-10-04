13fc1239cc10be4597e40b8f3067df8b22af70b6
alexr415 <121706778+alexr415@users.noreply.github.com>
2024-10-03 17:15:52 -0700

    added /login route -finds and returns user from username and email params added /deleteAcc -deletes account using username and email params -rturns 204 if successful, 405 if not found

--- src/main/java/com/proj2g17/proj2g17/api/controller/UserController.java
+++ src/main/java/com/proj2g17/proj2g17/api/controller/UserController.java
@@ -3,10 +3,11 @@ package com.proj2g17.proj2g17.api.controller;
 import com.proj2g17.proj2g17.api.model.User;
 import com.proj2g17.proj2g17.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -22,4 +23,29 @@ public class UserController {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
+    @PostMapping("/login")
+    public User login(@RequestParam String email, @RequestParam String username) {
+        Optional<User> userOptional = userRepository.findByEmailAndUsername(email, username);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        } else {
+            User newUser = new User();
+            newUser.setEmail(email);
+            newUser.setUsername(username);
+            newUser.setIs_admin(false); // default value
+            return userRepository.save(newUser);
+        }
+
+    }
+    @DeleteMapping("/deleteAcc")
+    public ResponseEntity<Void> deleteAccount(@RequestParam String email, @RequestParam String username) {
+        Optional<User> userOptional = userRepository.findByEmailAndUsername(email, username);
+        if (userOptional.isPresent()) {
+            userRepository.delete(userOptional.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
