34ac2957f82d9df0bb502735a8a2ce2b7709a2fc
Judah Silva <silva.judah7@outlook.com>
2024-10-10 11:47:17 -0700

    Add two admin routes

--- src/main/java/com/proj2g17/proj2g17/api/controller/UserController.java
+++ src/main/java/com/proj2g17/proj2g17/api/controller/UserController.java
@@ -3,8 +3,10 @@ package com.proj2g17.proj2g17.api.controller;
 import com.proj2g17.proj2g17.api.model.User;
 import com.proj2g17.proj2g17.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,10 +21,52 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    // ADMIN FUNCTIONS
+
+    private Boolean validateAdmin(Integer user_id) {
+        Optional<User> userOptional = userRepository.findById(user_id);
+        if (!userOptional.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Provided user_id is not a valid id");
+        }
+
+        if (!userOptional.get().getIs_admin()) {
+            return false;
+        }
+        
+        return true;
+    }
+    
     @GetMapping("/users")
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers(@RequestParam Integer user_id) {
+        if (!validateAdmin(user_id)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Provided user_id is not associated with an admin account");
+        }
+        
         return userRepository.findAll();
     }
+
+    @PutMapping("/createUser")
+    public ResponseEntity<Void> createNewUser(@RequestParam Integer user_id, @RequestParam String email, @RequestParam String username) {
+        if (!validateAdmin(user_id)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Provided user_id is not associated with an admin account");
+        }
+
+        Optional<User> userOptional = userRepository.findByEmailAndUsername(email, username);
+        if (!userOptional.isPresent()) {
+            User newUser = new User();
+            newUser.setEmail(email);
+            newUser.setUsername(username);
+            newUser.setIs_admin(false);
+            userRepository.save(newUser);
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+    // USER FUNCTIONS
+
     @PostMapping("/login")
     public User login(@RequestParam String email, @RequestParam String username) {
         Optional<User> userOptional = userRepository.findByEmailAndUsername(email, username);
@@ -46,10 +90,6 @@ public class UserController {
         } else {
             return ResponseEntity.notFound().build();
         }
-    }
-
-    public Optional<User> getByUser_id(Integer user_id) {
-        return userRepository.findById(user_id);
     }
 
 }
