3460e3a510ac4e05183966214f9d20bf1f0e6225
JudahSilva <jusilva@csumb.edu>
2024-10-14 23:54:50 -0700

    Finish two more admin routes

--- src/main/java/com/proj2g17/proj2g17/api/controller/UserController.java
+++ src/main/java/com/proj2g17/proj2g17/api/controller/UserController.java
@@ -3,6 +3,7 @@ package com.proj2g17.proj2g17.api.controller;
 import com.proj2g17.proj2g17.api.model.User;
 import com.proj2g17.proj2g17.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,15 +26,11 @@ public class UserController {
 
     private Boolean validateAdmin(Integer user_id) {
         Optional<User> userOptional = userRepository.findById(user_id);
-        if (!userOptional.isPresent()) {
+        if (userOptional.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Provided user_id is not a valid id");
         }
 
-        if (!userOptional.get().getIs_admin()) {
-            return false;
-        }
-        
-        return true;
+        return userOptional.get().getIs_admin();
     }
     
     @GetMapping("/users")
@@ -46,24 +43,57 @@ public class UserController {
     }
 
     @PutMapping("/createUser")
-    public ResponseEntity<Void> createNewUser(@RequestParam Integer user_id, @RequestParam String email, @RequestParam String username) {
+    public ResponseEntity<String> createNewUser(@RequestParam Integer user_id, @RequestParam String email, @RequestParam String username) {
         if (!validateAdmin(user_id)) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Provided user_id is not associated with an admin account");
         }
 
         Optional<User> userOptional = userRepository.findByEmailAndUsername(email, username);
-        if (!userOptional.isPresent()) {
+        if (userOptional.isEmpty()) {
             User newUser = new User();
             newUser.setEmail(email);
             newUser.setUsername(username);
             newUser.setIs_admin(false);
             userRepository.save(newUser);
+            return ResponseEntity.ok("User created");
         }
 
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists");
     }
 
+    @DeleteMapping("/deleteUser")
+    public ResponseEntity<String> deleteUser(@RequestParam Integer user_id, @RequestParam String email, @RequestParam String username) {
+        if (!validateAdmin(user_id)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Provided user_id is not associated with an admin account");
+        }
 
+        Optional<User> userOptional = userRepository.findByEmailAndUsername(email, username);
+        if (userOptional.isPresent()) {
+            userRepository.delete(userOptional.get());
+            return ResponseEntity.ok("User successfully deleted");
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PatchMapping("/updateUser")
+    public ResponseEntity<String> updateUser(@RequestParam Integer user_id, @RequestParam String email, @RequestParam String username, @RequestParam Optional<String> newEmail, @RequestParam Optional<String> newUsername, @RequestParam Optional<Boolean> isAdmin) {
+        if (!validateAdmin(user_id)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Provided user_id is not associated with an admin account");
+        }
+
+        Optional<User> userOptional = userRepository.findByEmailAndUsername(email, username);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setEmail(newEmail.orElseGet(user::getEmail));
+            user.setUsername(newUsername.orElseGet(user::getUsername));
+            user.setIs_admin(isAdmin.orElseGet(user::getIs_admin));
+            userRepository.save(user);
+            return ResponseEntity.ok("User successfully updated");
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 
     // USER FUNCTIONS
 
@@ -79,14 +109,14 @@ public class UserController {
             newUser.setIs_admin(false); // default value
             return userRepository.save(newUser);
         }
-
     }
+
     @DeleteMapping("/deleteAcc")
-    public ResponseEntity<Void> deleteAccount(@RequestParam String email, @RequestParam String username) {
+    public ResponseEntity<String> deleteAccount(@RequestParam String email, @RequestParam String username) {
         Optional<User> userOptional = userRepository.findByEmailAndUsername(email, username);
         if (userOptional.isPresent()) {
             userRepository.delete(userOptional.get());
-            return ResponseEntity.noContent().build();
+            return ResponseEntity.ok("User successfully deleted");
         } else {
             return ResponseEntity.notFound().build();
         }
