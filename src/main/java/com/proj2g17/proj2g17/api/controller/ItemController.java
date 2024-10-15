a13aecaed6d3e9076c05053227051d2bd508e8aa
Judah Silva <silva.judah7@outlook.com>
2024-10-15 11:15:38 -0700

    Add route for editing items

--- src/main/java/com/proj2g17/proj2g17/api/controller/ItemController.java
+++ src/main/java/com/proj2g17/proj2g17/api/controller/ItemController.java
@@ -3,9 +3,8 @@ package com.proj2g17.proj2g17.api.controller;
 import com.proj2g17.proj2g17.api.model.Item;
 import com.proj2g17.proj2g17.api.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -32,6 +31,30 @@ public class ItemController {
         return itemOptional.orElseGet(Collections::emptyList);
     }
 
+    @PatchMapping("/editItem")
+    public ResponseEntity<Item> editItem(
+        @RequestParam Integer item_id,
+        @RequestParam Optional<String> url,
+        @RequestParam Optional<String> name,
+        @RequestParam Optional<Double> price,
+        @RequestParam Optional<String> seller,
+        @RequestParam Optional<String> image_url
+    ) {
+        Optional<Item> itemOptional = itemRepository.findById(item_id);
 
+        if (itemOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Item item = itemOptional.get();
+        item.setUrl(url.orElseGet(item::getUrl));
+        item.setName(name.orElseGet(item::getName));
+        item.setPrice(price.orElseGet(item::getPrice));
+        item.setSeller(seller.orElseGet(item::getSeller));
+        item.setImage_url(image_url.orElseGet(item::getImage_url));
+        itemRepository.save(item);
+
+        return ResponseEntity.ok(item);
+    }
 
 }
